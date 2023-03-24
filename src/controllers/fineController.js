2355b5e489a26d961ced1a1272344b3d0a8a1367
Desiree Correa <audny.correac@gmail.com>
2023-03-24 13:24:11 -0600

    add services to controller

--- src/controllers/fineController.js
+++ src/controllers/fineController.js
@@ -1,21 +1,27 @@
 const fineService = require("../services/fineService");
+
 const getAllFines = (req, res) => {
+  const allFines = fineService.getAllFines();
   res.send("Get all Fines");
 };
 
 const getFineById = (req, res) => {
+  const fine = fineService.getFineById(req.params.fineId);
   res.send(`Get the fine ${req.params.fineId}`);
 };
 
 const createNewFine = (req, res) => {
+  const createdFine = fineService.createNewFine(req.params.fineId);
   res.send(`Create new fine ${req.params.fineId}`);
 };
 
 const updateOneFine = (req, res) => {
+  const updatedFine = fineService.updateOneFine(req.params.fineId);
   res.send(`Update fine ${req.params.fineId}`);
 };
 
 const deleteOneFine = (req, res) => {
+  fineService.deleteOneFine(req.params.fineId);
   res.send(`Delete fine ${req.params.fineId}`);
 };
 
