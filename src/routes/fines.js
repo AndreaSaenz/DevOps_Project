f1873a89f9e1ab2ab876cec165c0ef73948f6768
Desiree Correa <audny.correac@gmail.com>
2023-03-23 22:37:04 -0600

    add methods get by id, put and delete

--- src/routes/fines.js
+++ src/routes/fines.js
@@ -10,4 +10,37 @@ router.get("/", function (request, response, next) {
   }
 });
 
+router.get("/:id", function (request, reponse, next) {
+  try {
+  } catch (error) {
+    console.error("Error in the GET BY ID method in fines");
+    next(error);
+  }
+});
+
+router.post("/", function (request, response, next) {
+  try {
+    response.send("Here is the POST method of fines");
+  } catch (error) {
+    console.error("Error in the POST method of fines", error.message);
+    next(error);
+  }
+});
+
+router.put("/:id", function (request, response, next) {
+  try {
+  } catch (error) {
+    console.error("Error in the PUT method of fines", error.message);
+    next(error);
+  }
+});
+
+router.delete("/:id", function (request, response, next) {
+  try {
+  } catch (error) {
+    console.error("Error in the DELETE method of fines", error.message);
+    next(error);
+  }
+});
+
 module.exports = router;
