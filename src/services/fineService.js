12d8933832c398c0daefc464db8634467ae55eab
Desiree Correa <audny.correac@gmail.com>
2023-03-24 00:04:30 -0600

    create fine service

---
+++ src/services/fineService.js
@@ -0,0 +1,15 @@
+const getAllFines = () => {
+  return;
+};
+const getFineById = () => {
+  return;
+};
+const createNewFine = () => {
+  return;
+};
+const updateOneFine = () => {
+  return;
+};
+const deleteOneFine = () => {
+  return;
+};
