be00265d31210dd4402f986c5474577372ee3efc
Desiree Correa <audny.correac@gmail.com>
2023-03-25 19:27:08 -0600

    separete from service

--- src/controllers/fineController.js
+++ src/controllers/fineController.js
@@ -2,27 +2,58 @@ const fineService = require("../services/fineService");
 
 const getAllFines = (req, res) => {
   const allFines = fineService.getAllFines();
-  res.send("Get all Fines");
+  res.send({ status: "200", data: allFines });
 };
 
 const getFineById = (req, res) => {
   const fine = fineService.getFineById(req.params.fineId);
-  res.send(`Get the fine ${req.params.fineId}`);
+  //res.send(`Get the fine ${req.params.fineId}`);
+  res.send({ status: "200", data: fine });
 };
 
 const createNewFine = (req, res) => {
-  const createdFine = fineService.createNewFine(req.params.fineId);
-  res.send(`Create new fine ${req.params.fineId}`);
+  const { body } = req;
+
+  // Validation for all the necessary info
+  if (
+    !body.fineId ||
+    !body.amount ||
+    !body.date ||
+    !body.remark ||
+    !body.loanId ||
+    !body.status
+  ) {
+    return { message: "You didn't submmit the necesary parameters. " };
+  }
+
+  const newFine = {
+    fineId: body.fineId,
+    amount: body.amount,
+    date: body.date,
+    remark: body.remark,
+    loanId: body.loanId,
+    status: body.status,
+  };
+  const createdFine = fineService.createNewFine(newFine);
+  res.status(201).send({ status: "OK", data: createdFine });
 };
 
 const updateOneFine = (req, res) => {
-  const updatedFine = fineService.updateOneFine(req.params.fineId);
-  res.send(`Update fine ${req.params.fineId}`);
+  const fineId = req.params.fineId;
+  const fineStatus = req.params.status;
+
+  if (!fineId || !fineStatus) {
+    return;
+  }
+  const updatedFine = fineService.updateOneFine(fineId, fineStatus);
+  //res.send(`Update fine ${req.params.fineId}`);
+  res.status(200).send({ status: "OK", data: updatedFine });
 };
 
 const deleteOneFine = (req, res) => {
   fineService.deleteOneFine(req.params.fineId);
-  res.send(`Delete fine ${req.params.fineId}`);
+  //res.send(`Delete fine ${req.params.fineId}`);
+  res.send({ status: "OK", message: "Fine Deleted" });
 };
 
 module.exports = {
