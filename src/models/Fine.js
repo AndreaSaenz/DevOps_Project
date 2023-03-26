d82351b906db117ed0352c869ed7c19f98612173
Desiree Correa <audny.correac@gmail.com>
2023-03-25 19:25:11 -0600

    add POST, PUT and DELETE

--- src/models/Fine.js
+++ src/models/Fine.js
@@ -1,16 +1,54 @@
-const { DataTypes } = require("sequelize");
+const db = require("./dbTest.json");
 
-function Fine(sequelize) {
-  const attributes = {
-    fineId: { type: DataTypes.SMALLINT, allowNull: false },
-    amount: { type: DataTypes.INTEGER, allowNull: false },
-    date: { type: DataTypes.DATE, allowNull: false },
-    remark: { type: DataTypes.TEXT, allowNull: false },
-    loanId: { type: DataTypes.SMALLINT, allowNull: false },
-    status: { type: DataTypes.BOOLEAN, allowNull: false },
-  };
+// const { DataTypes } = require("sequelize");
 
-  return sequelize.define("Fine", attributes);
-}
+// function Fine(sequelize) {
+//   const attributes = {
+//     fineId: { type: DataTypes.SMALLINT, allowNull: false },
+//     amount: { type: DataTypes.INTEGER, allowNull: false },
+//     date: { type: DataTypes.DATE, allowNull: false },
+//     remark: { type: DataTypes.TEXT, allowNull: false },
+//     loanId: { type: DataTypes.SMALLINT, allowNull: false },
+//     status: { type: DataTypes.BOOLEAN, allowNull: false },
+//   };
 
-module.exports = Fine;
+//   return sequelize.define("Fine", attributes);
+// }
+
+const getAllFines = () => {
+  return db.fines;
+};
+
+const getFineById = (searchedFineId) => {
+  const oneFine = db.fines.find((fine) => fine.fineId === searchedFineId);
+  return oneFine;
+};
+
+const createANewFine = (newFine) => {
+  const isAlreadyAdded = db.fines.find(
+    (fine) => fine.fineId === newFine.fineId
+  );
+
+  if (!isAlreadyAdded) {
+    //function to save the info in the db
+  } else {
+    return { message: "This Fine already exists" };
+  }
+};
+
+const updateFine = (fineId, fineStatus) => {
+  //function to update fine in db
+  return;
+};
+
+const deleteFine = (fineId) => {
+  //function to delete fine
+  return;
+};
+module.exports = {
+  getAllFines,
+  getFineById,
+  createANewFine,
+  updateFine,
+  deleteFine,
+};
