b89d1d63217950c82253eeade1297dc77286943d
Desiree Correa <audny.correac@gmail.com>
2023-03-28 12:07:01 -0600

    add user registration service

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,8 +1,20 @@
+const { Sequelize } = require("sequelize");
 const User = require("../models/User");
 
-const registerNewUser = (newUser) => {
-  const newUser = User.createNewUser(newUser);
-  return newUser;
+const registerNewUser = async (newUser) => {
+  try {
+    const foundUser = await User.findOne({ where: { email: newUser.email } });
+    if (foundUser) {
+      throw {
+        status: 400,
+        message: "User already registered",
+      };
+    }
+    const createdUser = await User.create(newUser);
+    return createdUser;
+  } catch (error) {
+    throw { status: error?.status || 500, message: error?.message || error };
+  }
 };
 
 module.exports = {
