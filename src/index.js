4275c70299fec51c1972c658586c6d38066889a7
AndreaSaenz <andrea.inteligente1979@gmail.com>
2023-03-28 04:39:37 -0600

    Editando index.js para probar entidad Loan

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const loanRouter = require("./routes/loanRoutes"); 
+const db = require("./database/config");
+const app = express()
+
+const PORT = 3000
+
+(async () => {
+  try {
+    await db.authenticate();
+    //await db.sync();
+    console.log("Connection with database successful");
+  } catch (error) {
+    throw error;
+  }
+})()
+
+app.use(express.json());
+
+app.use("/loans", loanRouter);
+
+app.listen(PORT, () => {
+  console.log(`DevOps_Project listening on port ${PORT}`)
+})
