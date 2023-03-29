f3586997152961983f9ccf2e9bed7aac0c07a7f6
AndreaSaenz <andrea.inteligente1979@gmail.com>
2023-03-29 07:32:52 -0600

    Cambios en la config y sincronizacion de la db

--- src/database/config.js
+++ src/database/config.js
@@ -1,4 +1,4 @@
-
+require('dotenv').config();
 const {Sequelize} = require('sequelize');
 //const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = require('../config');
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const Server = require('./models/server');
+/*const Server = require('./models/server');
 const server = new Server();
 
-server.listen();
+server.listen();*/
 
 
 /*const express = require('express');
@@ -12,3 +12,28 @@ const app = express();
 app.listen(3000, () => {
     console.log('server on port 3000.')
 })*/
+
+const express = require('express')
+const studentRouter = require("./routes/studentRoutes"); 
+const db = require("./database/config");
+const app = express()
+
+const PORT = 3000;
+
+(async () => {
+  try {
+    await db.authenticate();
+    await db.sync( { alter: true } );
+    console.log("Connection with database successful");
+  } catch (error) {
+    throw error;
+  }
+})()
+
+app.use(express.json());
+
+app.use("/students", studentRouter);
+
+app.listen(PORT, () => {
+  console.log(`DevOps_Project listening on port ${PORT}`)
+})
