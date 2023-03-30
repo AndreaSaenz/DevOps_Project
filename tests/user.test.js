0baa7fbe144215185215fe87829cce3a072d41ad
Desiree Correa <audny.correac@gmail.com>
2023-03-30 11:30:05 -0600

    add jest dependency

---
+++ tests/user.test.js
@@ -0,0 +1,10 @@
+const {} = require("../src/controllers/User");
+describe("Test user endpoints", () => {
+  it("should all the register service", async () => {
+    const newUser = {
+      userName: "JaneDoe",
+      email: "janeDoe@example.test",
+      password: "janespass",
+    };
+  });
+});
