f8a6191e38dabedcbb84197e70f673a07626852e
Desiree Correa <audny.correac@gmail.com>
2023-03-30 13:38:33 -0600

    add unit test

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,10 +1,13 @@
-const {} = require("../src/controllers/User");
-describe("Test user endpoints", () => {
-  it("should all the register service", async () => {
-    const newUser = {
-      userName: "JaneDoe",
-      email: "janeDoe@example.test",
-      password: "janespass",
-    };
-  });
-});
+// const userController = require("../src/controllers/userController");
+// const userService = require("../src/services/userServices");
+
+// describe("Test user endpoints", () => {
+//   it("should all the register service", async () => {
+//     const newUser = {
+//       userName: "JaneDoe",
+//       email: "janeDoe@example.test",
+//       password: "janespass",
+//     };
+//     jest.spy;
+//   });
+// });
