a534f62b554f5627f20783aaabb1d83a90da1e93
Desiree Correa <audny.correac@gmail.com>
2023-05-10 19:20:48 -0600

    add logger to student

--- src/controllers/studentController.js
+++ src/controllers/studentController.js
@@ -1,37 +1,50 @@
 const studentService = require("../services/studentServices");
+const logger = require("../logger");
 
 const getAllStudents = async (req, res) => {
+  logger.debug("getAllStudents at studentController ");
   try {
     const allStudents = await studentService.getAllStudents();
     res.status(200).json(allStudents);
+    logger.info(`VERB: ${req.method} - HEADERS: ${req.headers.authorization}`);
+    logger.debug(`Method: getAlltudents - URL: ${req.originalUrl}`);
   } catch (error) {
     res
       .status(error?.status || 500)
       .json({ status: "FAILED", data: { error: error?.message || error } });
+    logger.error(error);
   }
 };
 
 const getStudentById = async (req, res) => {
+  logger.debug("getStudentById at studentController ");
   const studentId = req.params.studentId;
 
   if (!studentId) {
     res
       .status(400)
       .json({ status: "FAILED", data: { error: "studentId not indicated" } });
+    logger.warn("MISSING_PARAMETER: studentId");
     return;
   }
 
   try {
     const foundStudent = await studentService.getStudentById(studentId);
     res.status(200).json(foundStudent);
+    logger.info(
+      `VERB: ${req.method} - PARAMS:{ fineId: ${req.params.fineId} } - HEADERS: ${req.headers.authorization}`
+    );
+    logger.debug(`Method: getStudentById - URL: ${req.originalUrl}`);
   } catch (error) {
     res
       .status(error?.status || 500)
       .json({ status: "FAILED", data: { error: error?.message || error } });
+    logger.error(error);
   }
 };
 
 const createNewStudent = async (req, res) => {
+  logger.debug("createStudent at studentController ");
   const { body } = req;
 
   if (
@@ -41,9 +54,11 @@ const createNewStudent = async (req, res) => {
     !body.licenciatura ||
     !body.semestre
   ) {
-    res
-      .status(400)
-      .json({ status: "FAILED", data: { error: "Some parameters are missing" } });
+    res.status(400).json({
+      status: "FAILED",
+      data: { error: "Some parameters are missing" },
+    });
+    logger.warn("MISSING SOME BODY ELEMENTS");
     return;
   }
 
@@ -58,14 +73,20 @@ const createNewStudent = async (req, res) => {
 
     const createdStudent = await studentService.createNewStudent(newStudent);
     res.status(201).json({ status: "OK", data: createdStudent });
+    logger.info(`VERB: ${req.method} - HEADERS: ${req.headers.authorization}`);
+    logger.debug(
+      `Method: createNewStudent - URL: ${req.originalUrl} - BODY: {${req.body}}`
+    );
   } catch (error) {
     res
       .status(error?.status || 500)
       .json({ status: "FAILED", data: { error: error?.message || error } });
+    logger.error(error);
   }
 };
 
 const updateOneStudent = async (req, res) => {
+  logger.debug("updateOneStudent at studentController ");
   const studentId = req.params.studentId;
   const { body } = req;
 
@@ -73,46 +94,63 @@ const updateOneStudent = async (req, res) => {
     res
       .status(400)
       .json({ status: "FAILED", data: { error: "studentId not indicated" } });
+    logger.warn("MISSING_PARAMETER: studentId");
     return;
   }
 
-  if (
-    !body.Name ||
-    !body.email
-  ) {
-    res
-      .status(400)
-      .json({ status: "FAILED", data: { error: "Name and email can't be null" }});
+  if (!body.Name || !body.email) {
+    res.status(400).json({
+      status: "FAILED",
+      data: { error: "Name and email can't be null" },
+    });
+    logger.warn("MISSING SOME BODY ELEMENTS");
     return;
   }
 
   try {
-    const updatedStudent = await studentService.updateOneStudent(studentId, body);
+    const updatedStudent = await studentService.updateOneStudent(
+      studentId,
+      body
+    );
     res.status(200).json(updatedStudent);
+    logger.info(
+      `VERB: ${req.method} - PARAMS: fineId: ${req.params.fineId} - HEADERS: ${req.headers.authorization}`
+    );
+    logger.debug(
+      `Method: updateOneStudent - URL: ${req.originalUrl} - BODY: { ${req.body} }`
+    );
   } catch (error) {
     res
       .status(error?.status || 500)
       .json({ status: "FAILED", data: { error: error?.message || error } });
+    logger.error(error);
   }
 };
 
 const deleteOneStudent = async (req, res) => {
+  logger.debug("deleteOneStudent at studentController ");
   const studentId = req.params.studentId;
 
   if (!studentId) {
     res
       .status(400)
       .json({ status: "FAILED", data: { error: "studentId not indicated" } });
+    logger.warn("MISSING_PARAMETER: studentId");
     return;
   }
 
   try {
     await studentService.deleteOneStudent(studentId);
     res.status(204).json({ status: "OK", message: "Student deleted" });
+    logger.info(
+      `VERB: ${req.method} - PARAMS: {fineId: ${req.params.fineId}} - HEADERS: ${req.headers.authorization}`
+    );
+    logger.debug(`Method: deleteOneStudent - URL: ${req.originalUrl}`);
   } catch (error) {
     res
       .status(error?.status || 500)
       .json({ status: "FAILED", data: { error: error?.message || error } });
+    logger.error(error);
   }
 };
 
