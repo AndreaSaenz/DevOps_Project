cc4d9625445731192244d4c81ccc009c82439a0a
Desiree Correa <audny.correac@gmail.com>
2023-05-10 20:56:50 -0600

    made data ofuscated

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,8 +2,11 @@ const { Sequelize } = require("sequelize");
 const bcrypt = require("bcrypt");
 const userService = require("../services/userServices");
 const jwt = require("jsonwebtoken");
+const MaskData = require("maskdata");
+const logger = require("../logger");
 
 const register = async (req, res) => {
+  logger.debug("register at userController");
   const { body } = req;
 
   if (!(body.userName && body.email && body.password)) {
@@ -11,6 +14,7 @@ const register = async (req, res) => {
       status: "FAILED",
       data: { error: "Some parameters are missing" },
     });
+    logger.warn("MISSING SOME BODY ELEMENTS");
   }
 
   encryptedPassword = await bcrypt.hash(body.password, 10);
@@ -21,20 +25,31 @@ const register = async (req, res) => {
       email: body.email,
       password: encryptedPassword,
     };
+
     const createdUser = await userService.registerNewUser(newUser);
     const token = jwt.sign({ user: createdUser }, "secretKey");
+    const maskPassword = MaskData.maskPassword(body.password);
+    const maskedEmail = MaskData.maskEmail2(body.email);
+
     res.status(201).send({ status: "OK", data: createdUser, token: token });
+    logger.info(`VERB: ${req.method} - HEADERS: ${req.headers.authorization}`);
+    logger.debug(
+      `Method: registerNewUser - URL: ${req.originalUrl} - BODY: {{userName: ${body.userName}, email: ${maskedEmail}, password: ${maskPassword}}`
+    );
   } catch (error) {
     res
       .status(error?.status || 500)
       .json({ status: "FAILED", data: { error: error?.message || error } });
+    logger.error(error);
   }
 };
 
 const logIn = async (req, res) => {
+  logger.debug("logIn at userController");
   const { body } = req;
   if (!(body.userName && body.password)) {
     res.status(400).send("User name and password are required");
+    logger.warn("MISSING SOME BODY ELEMENTS");
   }
   try {
     const user = await userService.logInUser(body.userName, body.password);
@@ -44,10 +59,16 @@ const logIn = async (req, res) => {
       message: "Welcome ",
       token: token,
     });
+    const maskPassword = MaskData.maskPassword(body.password);
+    logger.info(`VERB: ${req.method} - HEADERS: ${req.headers.authorization}`);
+    logger.debug(
+      `Method: logInUser - URL: ${req.originalUrl} - BODY: {{userName: ${body.userName}, password: ${maskPassword}}}`
+    );
   } catch (error) {
     res
       .status(error?.status || 500)
       .json({ status: "FAILED", data: { error: error?.message || error } });
+    logger.error(error);
   }
 };
 
