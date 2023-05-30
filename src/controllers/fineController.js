e636a7791541ab03cce95308b00588fb9f2c6349
IsaacEhuan <isaacehuanavila64f@gmail.com>
2023-05-30 03:34:10 -0600

    Cambio de "!body.estado" a "body.estado==null"

--- src/controllers/fineController.js
+++ src/controllers/fineController.js
@@ -1,47 +1,152 @@
-const fineService = require("../services/fineService");
+const fineService = require("../services/fineServices");
+const logger = require("../logger");
 
 const getAllFines = async (req, res) => {
-  const allFines = await fineService.getAllFines();
-  res.send({ status: "200", data: allFines });
+  try {
+    const allFines = await fineService.getAllFines();
+    res.status(200).json(allFines);
+    logger.info(
+      `VERB: ${req.method}; QUERY_PARAMS: ${req.params}; HEADERS: Authorization: ${req.headers.authorization}`
+    );
+    logger.debug(
+      `Method: getAllFines; Query_Method: findAll(); URL: ${req.originalUrl}`
+    );
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .json({ status: "FAILED", data: { error: error?.message || error } });
+    logger.error(error);
+  }
 };
 
 const getFineById = async (req, res) => {
-  const fine = await fineService.getFineById(req.params.fineId);
-  res.send({ status: "200", data: fine });
+  logger.debug("getFineById at fineController ");
+  const fineId = req.params.fineId;
+
+  if (!fineId) {
+    res
+      .status(400)
+      .json({ status: "FAILED", data: { error: "fineId not indicated" } });
+    logger.warn("MISSING_PARAMETERS: fineId");
+    return;
+  }
+
+  try {
+    const foundFine = await fineService.getFineById(fineId);
+    res.status(200).json(foundFine);
+    logger.info(
+      `VERB: ${req.method}; QUERY_PARAMS:{ fineId: ${req.params.fineId} }; HEADERS: Authorization: ${req.headers.authorization}`
+    );
+    logger.debug(
+      `Method: getFineById; Query_Method: findByPk(); URL: ${req.originalUrl}`
+    );
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .json({ status: "FAILED", data: { error: error?.message || error } });
+    logger.error(error);
+  }
 };
 
 const createNewFine = async (req, res) => {
   const { body } = req;
 
-  // Validation for all the necessary info
-  if (!(body.monto && body.observacion && body.folioSolictud && body.estado)) {
-    return { message: "You didn't submmit the necesary parameters. " };
+  if (!body.monto || !body.folioSolictud) {
+    res.status(400).json({
+      status: "FAILED",
+      data: { error: "Some parameters are missing" },
+    });
+    logger.warn("MISSING SOME BODY ELEMENTS");
+    return;
   }
 
-  const newFine = {
-    monto: body.monto,
-    observacion: body.observacion,
-    folioSolicitud: body.folioSolicitud,
-    estado: body.estado,
-  };
-  const createdFine = await fineService.createNewFine(newFine);
-  res.status(201).send({ status: "OK", data: createdFine });
+  try {
+    const newFine = {
+      monto: body.monto,
+      observacion: body.observacion,
+      folioSolicitud: body.folioSolicitud,
+      estado: body.estado,
+    };
+
+    const createdFine = await fineService.createNewFine(newFine);
+    res.status(201).json({ status: "OK", data: createdFine });
+    logger.info(
+      `VERB: ${req.method}; QUERY_PARAMS: ${req.params}; HEADERS: Authorization: ${req.headers.authorization}`
+    );
+    logger.debug(
+      `Method: createNewFine; Query_Method: create(); URL: ${req.originalUrl}; BODY: {${req.body}}`
+    );
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .json({ status: "FAILED", data: { error: error?.message || error } });
+    logger.error(error);
+  }
 };
 
 const updateOneFine = async (req, res) => {
   const fineId = req.params.fineId;
   const { body } = req;
 
-  if (!fineId || !body.estado) {
-    return { message: "Enter the fine id and nw status" };
+  if (!fineId) {
+    res
+      .status(400)
+      .json({ status: "FAILED", data: { error: "fineId not indicated" } });
+    logger.warn("MISSING_PARAMETERS: fineId");
+    return;
   }
-  const updatedFine = await fineService.updateOneFine(fineId, body.estado);
-  res.status(200).send({ status: "OK", data: updatedFine });
+
+  if (body.estado==null) {
+    res
+      .status(400)
+      .json({ status: "FAILED", data: { error: "Status can't be null" } });
+    logger.warn("MISSING_BODY: estado");
+    return;
+  }
+
+  try {
+    const updatedFine = await fineService.updateOneFine(fineId, body);
+    res.status(200).json(updatedFine);
+    logger.info(
+      `VERB: ${req.method}; QUERY_PARAMS: fineId: ${req.params.fineId}; HEADERS: Authorization: ${req.headers.authorization}`
+    );
+    logger.debug(
+      `Method: updateOneFine; Query_Method: update(); URL: ${req.originalUrl}; BODY: { ${req.body} }`
+    );
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .json({ status: "FAILED", data: { error: error?.message || error } });
+    logger.error(error);
+  }
 };
 
 const deleteOneFine = async (req, res) => {
-  await fineService.deleteOneFine(req.params.fineId);
-  res.send({ status: "OK", message: "Fine Deleted" });
+  const fineId = req.params.fineId;
+
+  if (!fineId) {
+    res
+      .status(400)
+      .json({ status: "FAILED", data: { error: "fineId not indicated" } });
+    logger.warn("MISSING_PARAMETERS: fineId");
+    return;
+  }
+
+  try {
+    await fineService.deleteOneFine(fineId);
+    res.status(204).json({ status: "OK", message: "Fine deleted" });
+    logger.info(
+      `VERB: ${req.method}; QUERY_PARAMS: {fineId: ${req.params.fineId}}; HEADERS: Authorization: ${req.headers.authorization}`
+    );
+    logger.debug(
+      `Method: deleteOneFine; Query_Method: destroy(); URL: ${req.originalUrl}`
+    );
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .json({ status: "FAILED", data: { error: error?.message || error } });
+    logger.error(error);
+  }
 };
 
 module.exports = {
